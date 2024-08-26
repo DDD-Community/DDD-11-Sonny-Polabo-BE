8eb9af83aca9c3f6d3121cdb9adf8acf11d4f887
dldmsql <yhy5913@gmail.com>
2024-08-26 19:39:07 +0900

    fix(POLABO-108): 누적 생성 수 조건절 수정

--- src/main/kotlin/com/ddd/sonnypolabobe/global/util/DiscordApiClient.kt
+++ src/main/kotlin/com/ddd/sonnypolabobe/global/util/DiscordApiClient.kt
@@ -1,6 +1,5 @@
 package com.ddd.sonnypolabobe.global.util
 
-import com.sun.jndi.ldap.LdapPoolManager.trace
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
